82bc312082661492833bb909ae0baac9c1a009dd
逝兮诚 <marx_luo@foxmail.com>
2017-09-01 18:39:32 +0800

    初次提交

---
+++ js/main.js
@@ -0,0 +1,13 @@
+﻿/** 我看视频 */
+var resolutionURL = "";
+function watchByWMXW() {
+	resolutionURL = "http://www.vipjiexi.com/tong.PHP?url=" + location.href;
+	watch();
+}
+function watchByWVIPJX() {
+	resolutionURL = "http://www.wmxz.wang/video.php?url=" + location.href;
+	watch();
+}
+function watch() {
+	location.href = resolutionURL;
+}
---
+++ popup.js
@@ -0,0 +1,15 @@
+﻿/** 我看视频 */
+var resolutionURL = "";
+function watchByWMXW() {
+	alert("111");
+	resolutionURL = "http://www.vipjiexi.com/tong.PHP?url=" + location.href;
+	watch();
+}
+function watchByWVIPJX() {
+	alert("111");
+	resolutionURL = "http://www.wmxz.wang/video.php?url=" + location.href;
+	watch();
+}
+function watch() {
+	location.href = resolutionURL;
+}
